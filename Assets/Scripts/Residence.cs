3c6210a240ceaff33e440e9f3852e861e18baf5a
afroholic78 <mickael.zerihoun@gmail.com>
2017-09-28 10:19:22 -0600

    Base script for residence

--- Assets/Scripts/Residence.cs
+++ Assets/Scripts/Residence.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Residence : MonoBehaviour {
+	public Vector2 location = new Vector2(0,0);
+	public int appearRate = 1;
+	public int dirtRate = 1;
+
+	public int assignedWorkers;
+	public bool isClean;
 
 	// Use this for initialization
 	void Start () {
-		
+		isClean = false;
+		assignedWorkers = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void Cleaned () {
+		isClean = true;
+	}
+	
+	private void getsDirty () {
+		isClean = false;
+	}
+
 }
